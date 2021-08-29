d0eadf77e3d7b9a79391c4c78bf77ed6f6db7522
Ajay Singh <ajay10mar96@gmail.com>
2021-08-29 15:30:55 +0530

    changing up env vars

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,11 +7,3 @@ export const DB_URL = process.env.DB_URL || null;
 export const APP_URL = process.env.APP_URL || `http://localhost:${PORT}`;
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const JWT_SECRET = process.env.JWT_SECRET || null;
-
-export default {
-  PORT,
-  DB_URL,
-  APP_URL,
-  NODE_ENV,
-  JWT_SECRET,
-};
