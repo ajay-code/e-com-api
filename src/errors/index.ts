e24620ad27f2f8cfe6af4f1f9bb30dd0153577bd
Ajay Singh <ajay10mar96@gmail.com>
2021-10-17 13:23:29 +0530

    changing not found

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,4 +1,4 @@
 export { default as CustomAPIError } from "./customAPIError";
-export { default as NotFoundError } from "./notFoundError";
+export { default as NotFoundError } from "./otFoundError";
 export { default as BadRequestError } from "./badRequestError";
 export { default as UnauthenticatedError } from "./unauthenticatedError";
